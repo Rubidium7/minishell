ee03832012229f08b2fb2edce3f43a6b2e38effd
Rubidium7 <kyllikki.fillman@gmail.com>
2023-09-14 13:48:59 +0300

    added complaining about no arguments

--- core/main.c
+++ core/main.c
@@ -12,13 +12,20 @@
 
 #include "minishell.h"
 
-void	add_to_history(char *str)
+static int	complain(void)
+{
+	ft_putendl_fd("Running scripts is not supported", 2);
+	ft_putendl_fd("Please upgrade to a better shell program", 2);
+	return (37);
+}
+
+static void	add_to_history(char *str)
 {
 	if (ft_strncmp(str, "", 1))
 		add_history(str);
 } //might be useless as a lone function
 
-void	readline_pretty(t_shell *core)
+static void	readline_pretty(t_shell *core)
 {
 	char	*tmp;
 
@@ -30,9 +37,9 @@ void	readline_pretty(t_shell *core)
 int main(int ac, char **av, char **ev)
 {
 	t_shell	core;
-	(void)ac;
-	(void)av;
 
+	if (ac != 1 && av[0])
+		return (complain());
 	set_start_data(&core, ev);
 	//print_ar(core.env);
 	while (core.cur_process.shroom_time)
