de87b413acc63b8ae36d2888c7ff87994bb69107
Valeria Vagapova <lera.vagapova@gmail.com>
2023-09-15 20:28:57 +0300

    add n flag to echo, acho done

--- built-ins/echo.c
+++ built-ins/echo.c
@@ -6,17 +6,37 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 21:26:14 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/09/15 19:31:16 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/09/15 20:28:15 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static	t_bool is_minus_n(const char *str)
+{
+	int	i;
+	
+	if (str[0] != '-' || str[1] != 'n')
+		return (FALSE);
+	i = 2;
+	while (str[i])
+	{
+		if (str[i] != 'n')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
 int	echo(t_command *command)
 {
-	int i;
+	int		i;
+	t_bool	no_newline;
 
 	i = 1;
+	no_newline = is_minus_n(command->cmd_ar[1]);
+	if (no_newline)
+		i++;
 	//no -n option here
 	while (command->cmd_ar[i])
 	{
@@ -25,6 +45,7 @@ int	echo(t_command *command)
 			printf(" ");
 		i++;
 	}
-	printf("\n");
+	if (!no_newline)
+		printf("\n");
 	return (0);
 }
