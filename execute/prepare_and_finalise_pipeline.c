bd6f8d88559f7a1d82c44578e736b6b613ad2504
Valeria Vagapova <lera.vagapova@gmail.com>
2023-09-18 20:53:17 +0300

    fix crashing with 1000+ pipes

--- execute/prepare_and_finalise_pipeline.c
+++ execute/prepare_and_finalise_pipeline.c
@@ -6,33 +6,50 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:23:41 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/09/18 14:12:29 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/09/18 20:50:04 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	init_pipes(int **pipes, int len)
+{
+	int i;
+
+	i = 0;
+	while (i < len)
+	{
+		pipes[i][0] = -1;
+		pipes[i][1] = -1;
+		i++;
+	}
+	//ft_putnbr_fd(i, 2);
+	//ft_putstr_fd(" - i at which init pipes stopped\n", 2);
+}
+
 // Pipe and child init
 int	prepare_pipes_and_children(t_shell *core, int ***pipes, pid_t **children,
 int len)
 {
+	*children = NULL;
 	*pipes = malloc_pipes(len - 1);
 	if (!(*pipes))
 	{
+		//ft_putstr_fd("malloc failed\n", 2);
 		core->cur_process.error_index = MALLOC_FAIL;
 		return (MALLOC_FAIL);
 	}
+	init_pipes(*pipes, len - 1);
 	if (open_pipes(*pipes) != SUCCESS)
 	{
+		//ft_putstr_fd("opening pipes failed\n", 2);
 		core->cur_process.error_index = PIPE_FAIL;
-		free(pipes);
 		return (PIPE_FAIL);
 	}
 	*children = ft_calloc(sizeof(pid_t), len);
 	if (!(*children))
 	{
 		core->cur_process.error_index = MALLOC_FAIL;
-		free(pipes);
 		return (MALLOC_FAIL);
 	}
 	return (SUCCESS);
@@ -43,6 +60,8 @@ static int	wait_for_children(pid_t *children, int len)
 	int	i;
 	int	ret;
 
+	if (!children)
+		return (0);
 	i = 0;
 	ret = 0;
 	while (i < len)
