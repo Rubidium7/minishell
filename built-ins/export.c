2947e6f060e3f667dd42ff579a8b96dc5f9ffc13
Valeria Vagapova <lera.vagapova@gmail.com>
2023-09-18 19:17:31 +0300

    make export and unset printf all errors

--- built-ins/export.c
+++ built-ins/export.c
@@ -6,7 +6,7 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 21:26:23 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/09/18 18:52:08 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:15:29 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,17 @@
 int	export(t_shell *core, t_command *command)
 {
 	int	i;
-	int	first_error_ret;
 	int	ret;
 
 	i = 1;
-	first_error_ret = 0;
+	ret = 0;
 	while (command->cmd_ar[i])
 	{
-		ret = add_env_from_string(core, command->cmd_ar[i]);
-		if (ret && !first_error_ret)
-			first_error_ret = ret;
+		if (add_env_from_string(core, command->cmd_ar[i]))
+			ret = 1;
 		i++;
 	}
 	if (i == 1)
 		print_envs(EXPORT, core);
-	return (first_error_ret);
+	return (ret);
 }
--- built-ins/unset.c
+++ built-ins/unset.c
@@ -6,7 +6,7 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 21:26:36 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/09/18 19:09:49 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:16:12 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,24 @@ t_bool	is_valid_key(const char *key)
 int	unset(t_shell *core, t_command *command)
 {
 	int	i;
-	int	first_error_ret;
+	int	ret;
 
 	i = 1;
-	first_error_ret = 0;
+	ret = 0;
 	while (command->cmd_ar[i])
 	{
-		if (!is_valid_key(command->cmd_ar[i]) && !first_error_ret)
+		if (!is_valid_key(command->cmd_ar[i]))
 		{
 			core->cur_process.error_index = INVALID_IDENTIFIER;
 			ft_putstr_fd(ERROR_SHROOM, 2);
 			ft_putstr_fd(" unset: `", 2);
 			ft_putstr_fd(command->cmd_ar[i], 2);
 			ft_putstr_fd("': not a valid identifier\n", 2);
-			first_error_ret = 1;
+			ret = 1;
 		}
-		else if (unset_env(command->cmd_ar[i], core) && !first_error_ret)
-			first_error_ret = 1;
+		else if (unset_env(command->cmd_ar[i], core) && !ret)
+			ret = 1;
 		i++;
 	}
-	return (first_error_ret);
+	return (ret);
 }
